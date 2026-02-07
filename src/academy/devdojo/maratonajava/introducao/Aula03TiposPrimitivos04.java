9bd13bd047ae7afb7e37e4b694810b08fe7fb11e
Vinicius Coelho <vvcglow.gg@gmail.com>
2026-02-07 13:28:46 -0300

    Primeiro commit do projeto Maratona Java

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivos04.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivos04 {
+    static void main() {
+        String nome = "Vinicius Murce Medeiros Coelho";
+        String endereco = "Street Bervelly Hills" ;
+        float salario = 2521.79f;
+        String dataRecebimento = "02/06/2026";
+        System.out.println("Eu "+nome+" morador do endereco "+endereco);
+        System.out.println("confirmo que recebi o salário de R$"+salario+" na data "+dataRecebimento);
+    }
+}
